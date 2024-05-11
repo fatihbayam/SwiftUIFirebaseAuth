98125f1e1fa2c909c049093d778e2d36f9224263
fatihbayam <fatihbayam@gmail.com>
2024-05-12 00:50:39 +0100

    Initial Commit

---
+++ SwiftUIFirebaseAuth/SwiftUIFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAuthApp.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Fatih Bayam on 12.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
