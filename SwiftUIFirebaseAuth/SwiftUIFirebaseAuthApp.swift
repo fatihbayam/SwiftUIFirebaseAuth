200f2a6a134e2130c73a364741fa0b5801b8e97f
fatihbayam <fatihbayam@gmail.com>
2024-05-12 01:33:17 +0100

    [Feature] Firebase configuration added

--- SwiftUIFirebaseAuth/SwiftUIFirebaseAuthApp.swift
+++ SwiftUIFirebaseAuth/SwiftUIFirebaseAuthApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct SwiftUIFirebaseAuthApp: App {
+    
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
+          NavigationView {
             ContentView()
+          }
         }
-    }
+      }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print("DEBUG: Firebase configured...")
+    return true
+  }
 }
