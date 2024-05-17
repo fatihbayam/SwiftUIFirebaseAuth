8267d6e31ebe9b7d697554fe161303d2a9d17b24
fatihbayam <fatihbayam@gmail.com>
2024-05-17 01:01:43 +0100

    [Feature] Sign In with Google Account and anonymous user linked to Google account functions added

---
+++ SwiftUIFirebaseAuth/Authentication/SignInGoogleHelper.swift
@@ -0,0 +1,8 @@
+//
+//  SignInGoogleHelper.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Fatih Bayam on 16.05.2024.
+//
+
+import Foundation
---
+++ SwiftUIFirebaseAuth/Utilities/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Fatih Bayam on 16.05.2024.
+//
+
+import Foundation
